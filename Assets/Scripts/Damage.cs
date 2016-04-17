4859449b55df0cbcc00496b529b151a94da546d1
Yuriy <uvesnin@gmail.com>
2016-04-17 02:21:50 -0700

    buatify

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class Damage : MonoBehaviour {
+public class Damage : NetworkBehaviour {
     public float amount = 0.21f;
+    public GameObject damage;
 
     void OnTriggerEnter2D (Collider2D col) {
         var getter = col.GetComponent<IDamagable>();
         if(getter != null) {
             getter.GetDamage(amount);
         }
+
+        Instantiate(damage, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
